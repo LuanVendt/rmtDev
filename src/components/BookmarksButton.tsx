46a98ff51381893962b1c0f0d87f4976bec2cea0
Luan Vendt <luan.vendt@bitize.com.br>
2025-01-29 15:45:19 -0300

    feat: implement useOnClickOutside hook for improved click handling; refactor BookmarksButton to utilize new hook

--- src/components/BookmarksButton.tsx
+++ src/components/BookmarksButton.tsx
@@ -1,25 +1,13 @@
 import { TriangleDownIcon } from "@radix-ui/react-icons";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
+import { useOnClickOutside } from "../lib/hooks/hooks";
 import BookmarksPopover from "./BookmarksPopover";
 
 export default function BookmarksButton() {
   const [isOpen, setIsOpen] = useState(false);
   const buttonRef = useRef<HTMLButtonElement>(null);
   const popoverRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const handleClick = (e: MouseEvent) => {
-      if (
-        e.target instanceof HTMLElement &&
-        !buttonRef.current?.contains(e.target)
-      )
-        setIsOpen(false);
-    };
-
-    document.addEventListener("click", handleClick);
-
-    return () => document.removeEventListener("click", handleClick);
-  }, []);
+  useOnClickOutside([buttonRef, popoverRef], () => setIsOpen(false));
 
   return (
     <section>
